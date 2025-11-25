9418e20c92a210a3ace915b217506203e2c9f7bf
Justin <justinw86420@gmail.com>
2025-11-25 10:01:42 -0500

    Plot MACD and price with MACD indicators

--- src/technical_indicators.py
+++ src/technical_indicators.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import ta
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 def get_historical_prices(ticker):
     # Retrieve stock price history from past year and 3 years
@@ -28,7 +29,6 @@ def get_rsi(ticker, history):
     rsi_plot.add_hline(y=30, line_dash='dash', line_color='green')
     rsi_plot.show()
     return rsi
-rsi = get_rsi(ticker=ticker, history=history3)
 
 def plot_rsi_price(ticker, history, r):
     plt.figure(figsize=(14, 8), dpi=120)
@@ -51,4 +51,41 @@ def plot_rsi_price(ticker, history, r):
     ax1.set_ylabel('RSI', color='red')
     ax2.set_ylabel('Price', color='green')
     plt.show()
-plot_rsi_price(ticker=ticker, history=history3, r=rsi)
+
+def get_macd(ticker, history):
+    # Obtain macd line, signal line, and histogram
+    macd_object = ta.trend.MACD(close=history.Close, window_slow=26, window_fast=12, window_sign=9)
+    macd = macd_object.macd()
+    signal_line = macd_object.macd_signal()
+    histogram = macd_object.macd_diff()
+    # Put into dataframe
+    macd_df = pd.DataFrame({'MACD': macd, 'Signal': signal_line, 'Histogram': histogram}, index=history.index)
+    #Plot
+    macd_plot = px.line(macd_df, x=macd_df.index, y=['MACD', 'Signal'], title=f"{ticker.info['shortName']} MACD", height=500)
+    macd_plot.update_layout(xaxis_title='Date', yaxis_title='MACD')
+    macd_plot.show()
+    return macd_df
+macd_df = get_macd(ticker=ticker, history=history1)
+
+
+def plot_price_indicators(m, ticker, history):
+    # Find indicators
+    history['bullish_signal'] = (m.MACD > m.Signal) & (m.MACD.shift(1) < m.Signal.shift(1))
+    history['bearish_signal'] = (m.MACD < m.Signal) & (m.MACD.shift(1) > m.Signal.shift(1))
+    buy_dates = history[history['bullish_signal']].index
+    sell_dates = history[history['bearish_signal']].index
+
+    # Plot closing price with indicators
+    price_signals = go.Figure()
+    price_signals.add_trace(go.Scatter(x=history.index, y=history.Close, mode='lines', name='Closing Price'))
+    price_signals.add_trace(
+        go.Scatter(x=buy_dates, y=history.loc[buy_dates, 'Close'], mode='markers', marker_symbol='triangle-up',
+                   marker_color='green', name='Buy Signal', marker_size=12))
+    price_signals.add_trace(
+        go.Scatter(x=sell_dates, y=history.loc[sell_dates, 'Close'], mode='markers', marker_symbol='triangle-down',
+                   marker_color='red', name='Sell Signal', marker_size=12))
+    price_signals.update_layout(yaxis_title='Price', xaxis_title='Date', height=600,
+                                title=f"{ticker.info['shortName']} Closing Price with MACD Indicators")
+    price_signals.show()
+
+plot_price_indicators(m=macd_df, ticker=ticker, history=history1)
