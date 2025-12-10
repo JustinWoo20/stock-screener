97162c3bcb7cc76d8d2305efc7a26fb521f72e3f
Justin <justinw86420@gmail.com>
2025-12-10 15:19:15 -0500

    Finish debugging web scraping

--- scraping/equity_field_params.py
+++ scraping/equity_field_params.py
@@ -1,4 +1,3 @@
-# TODO: Peer group
 import re
 import requests
 from bs4 import BeautifulSoup
@@ -6,32 +5,26 @@ from bs4 import BeautifulSoup
 # Fetch yfinance api page
 url = 'https://ranaroussi.github.io/yfinance/reference/api/yfinance.EquityQuery.html'
 response = requests.get(url)
-print(response.status_code)
 
 # Parse
 soup = BeautifulSoup(response.text, 'html.parser')
 
 table = soup.find(id='id2')
-body = table.find('tbody')
-
-# Final all p elements
-p_elements = body.find_all('p')
+tbody = table.find('tbody')
 
 # Find all div elements with the class line
-line_classes = body.find_all(class_='line')
-# Use splicing to obtain only exchanges
-exchange_divs = line_classes[:20]
-industry_divs = line_classes[20:]
-print(industry_divs)
+line_classes = tbody.find_all(class_='line')
 
 def get_regions():
     # Obtain possible regions from yfinance documentation
+    p_elements = tbody.find_all('p')
     text = p_elements[1].get_text()
     split_text = text.split(sep=', ')
     return split_text
 
 def get_exchanges():
     # Obtain possible exchanges from yfinance documentation
+    exchange_divs = line_classes[:20]
     exchange_list = []
     for e in exchange_divs:
         text = e.get_text()
@@ -43,25 +36,22 @@ def get_exchanges():
 
 def get_industries():
     # Get a dictionary of industries from yfinance website
+    industry_divs = line_classes[20:]
     sector_list = []
     industry_dict = {}
     for i in industry_divs:
         text = i.get_text(strip=True)
-        sector, industries = text.split(': ')
-        industries = industries.split(', ')
-        industry_dict[sector] = industries
+        sector, ind = text.split(': ')
+        industries_split = ind.split(', ')
+        industry_dict[sector] = industries_split
         sector_list.append(sector)
 
     return sector_list, industry_dict
 
-
-
-# import re
-# s = "Geeks, for; Geeks"
-#
-# # Using split() with a capturing group to keep the delimiters
-# result = re.split(r'(,|;)', s)
-#
-# print(result)
-#codes = re.findall(r'\b[A-Z]{2,}\b', text)
-
+def get_peer_groups():
+    # Obtain a dictionary of yfinance peer groups
+    simple_classes = tbody.find_all(class_='simple')
+    simple_cl_txt = simple_classes[1].get_text()
+    split_txt = simple_cl_txt.split('\n')
+    peer_group_list = split_txt[1:len(split_txt) - 1]
+    return peer_group_list
