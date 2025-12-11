c7ef432137c04f5246c3b9701dc395cd483a8d83
Justin <justinw86420@gmail.com>
2025-12-11 11:06:21 -0500

    Start writing screener params

--- server.py
+++ server.py
@@ -1,17 +1,19 @@
 # TODO: Hide dropdown menu until Add criteria is clicked
 from flask import Flask, render_template, redirect, url_for
-
+from scraping import equity_field_params
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return redirect(url_for('screener'))
-
 @app.route('/screener')
 def screener():
-    sectors = {'Equity Fields': {'exchange':['BUE', 'VIE', 'ASX', 'BRU',
-                                             'SAO', 'CNQ', 'NEO', 'TOR',
-                                             'VAN', 'EBS', 'SGO'] }}
+    regions = equity_field_params.get_regions()
+    print(regions)
+    exchanges = equity_field_params.get_exchanges()
+    sectors, industries = equity_field_params.get_industries()
+    peer_groups = equity_field_params.get_peer_groups()
+
     return render_template('screener.html')
 
 if __name__ == '__main__':
---
+++ src/screener_params.py
@@ -0,0 +1,33 @@
+price_metrics = {'price': [{'eodprice': 'End of the day price',
+                            'fiftytwowkpercentchange': '52 week percent change',
+                            'intradaymarketcap': 'Intra day market cap',
+                            'intradayprice': 'Intra day price',
+                            'intradaypricechange': 'Intra day price change',
+                            'lastclose52weekhigh.lasttwelvemonths': '12 month high',
+                            'lastclose52weeklow.lasttwelvemonths': '12 month low',
+                            'lastclosemarketcap.lasttwelvemonths': '12 month market cap',
+                            'percentchange': 'percent change',}]}
+
+trading_metrics = {'trading': [{'avgdailyvol3m': 'Average 3 month daily volume',
+                                'beta': 'Beta',
+                                'dayvolume': 'Day volume',
+                                'eodvolume': 'End of day volume',
+                                'pctheldinsider': 'Percent held by insiders',
+                                'pctheldinst': 'Percent held by institutions',}]}
+
+valuation_metrics = {'valuation': [{'bookvalueshare.lasttwelvemonths': 'Trailing BVPS',
+                                    'lastclosemarketcaptotalrevenue.lasttwelvemonths': 'Market cap to total revenue',
+                                    'lastclosepriceearnings.lasttwelvemonths': 'Trailing P/E',
+                                    'lastclosepricetangiblebookvalue.lasttwelvemonths': 'Trailing P/TBV',
+                                    'lastclosetevtotalrevenue.lasttwelvemonths': 'Enterprise Value to Sales ratio',
+                                    'pegratio_5y': '5 year PEG ratio',
+                                     'pricebookratio.quarterly': 'P/B ratio',}]}
+
+profitability_metrics = {'profitability': [{'consecutive_years_of_dividend_growth_count': 'Consecutive years of dividend growth',
+                                            'forward_dividend_per_share': 'Projected yearly dividend',
+                                            'forward_dividend_yield': 'Projected yearly dividend yield',
+                                            'returnonassets.lasttwelvemonths': 'Trailing ROA',
+                                            'returnonequity.lasttwelvemonths': 'Trailing ROE',
+                                            'returnontotalcapital.lasttwelvemonths': 'Trailing ROTC',}]}
+
+leverage_metrics = {'leverage': [{'ebitdainterestexpense.lasttwelvemonths': 'EBI'}]}
