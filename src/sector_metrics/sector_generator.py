e9a0fdfd90c73d54d4ce1d3fb22a0815262d7b13
Justin <justinw86420@gmail.com>
2026-03-09 20:02:11 -0400

    Create 'sector_generator.py' for making template sector data csv

---
+++ src/sector_metrics/sector_generator.py
@@ -0,0 +1,17 @@
+# Used to create columns in industry_labels.csv
+import csv
+from scraping.yfinance_scraper import get_industries
+
+file_header = ['', 'P/B', 'D/E', 'Y/Y', 'Revenue Growth', 'Gross Profit Margin', 'Trailing P/E', 'Forward P/E']
+
+sectors, industries = get_industries()
+industry_list =[]
+for industry in industries.values():
+    for i in industry:
+        industry_list.append(i)
+
+with open('../../data/blank_templates/sector_labels.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(file_header)
+    for item in industry_list:
+        writer.writerow([item])
