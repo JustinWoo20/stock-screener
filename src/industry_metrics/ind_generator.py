272098c7fe7999c790d8711eb3a673b7bfb5d744
Justin <justinw86420@gmail.com>
2026-03-09 20:00:53 -0400

    Fix csv path and typos

--- src/industry_metrics/ind_generator.py
+++ src/industry_metrics/ind_generator.py
@@ -1,6 +1,7 @@
 # Used to create columns in industry_labels.csv
 import csv
-from yfinance_scraper import get_industries
+from scraping.yfinance_scraper import get_industries
+file_header = ['', 'P/B', 'D/E', 'Y/Y', 'Revenue Growth', 'Gross Profit Margin', 'Trailing P/E', 'Forward P/E']
 
 sectors, industries = get_industries()
 industry_list =[]
@@ -8,7 +9,8 @@ for industry in industries.values():
     for i in industry:
         industry_list.append(i)
 
-with open('../data/industry_averages.csv', 'w', newline='') as csvfile:
+with open('../../data/blank_templates/industry_labels.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
+    writer.writerow(file_header)
     for item in industry_list:
         writer.writerow([item])
